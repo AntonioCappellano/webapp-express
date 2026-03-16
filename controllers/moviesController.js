2705eb9ff90e3437e10f2a76cc81d22589df8a08
AntonioCappellano <antoniocappellano97@gmail.com>
2026-03-16 17:13:47 +0100

    feat: add show controller

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,5 +1,8 @@
 const connection = require("../data/connDb");
-const { handleFailQuery } = require("../utils/databaseUtils");
+const {
+  handleFailQuery,
+  handleResourceNotFound,
+} = require("../utils/databaseUtils");
 
 function index(req, res) {
   const moviesSQL = "SELECT * FROM movies";
@@ -9,4 +12,22 @@ function index(req, res) {
   });
 }
 
-module.exports = { index };
+function show(req, res) {
+  const { id } = req.params;
+  const moviesSQL = "SELECT * FROM movies WHERE `id` = ?";
+  connection.query(moviesSQL, [id], (err, result) => {
+    if (err) return handleFailQuery(err, res);
+    const movie = result[0];
+    if (!movie) {
+      return handleResourceNotFound(res);
+    }
+    const reviewsSQL = "SELECT * FROM reviews WHERE movie_id = ?";
+    connection.query(reviewsSQL, [id], (err, reviewsResults) => {
+      if (err) return handleFailQuery(err, res);
+      movie.reviews = reviewsResults;
+      res.json({ movie });
+    });
+  });
+}
+
+module.exports = { index, show };
