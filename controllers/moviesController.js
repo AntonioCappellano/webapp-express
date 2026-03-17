6fd8a79f3c5623ef2cbad98fbf17be2dcec7cd0d
AntonioCappellano <antoniocappellano97@gmail.com>
2026-03-17 16:10:02 +0100

    add AVG vote and img path

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -5,10 +5,23 @@ const {
 } = require("../utils/databaseUtils");
 
 function index(req, res) {
-  const moviesSQL = "SELECT * FROM movies";
+  const moviesSQL = `
+  SELECT 
+       movies.*,
+       ROUND(AVG(reviews.vote)) avg_vote 
+  FROM 
+      movies
+  INNER JOIN reviews
+  ON movies.id = reviews.movie_id
+
+  GROUP BY movies.id`;
   connection.query(moviesSQL, (err, result) => {
     if (err) return handleFailQuery(err, res);
-    res.json({ result });
+
+    const movies = result.map((movie) => {
+      return { ...movie, image: buildMovieImgPath(movie.image) };
+    });
+    res.json({ result: movies });
   });
 }
 
@@ -25,9 +38,15 @@ function show(req, res) {
     connection.query(reviewsSQL, [id], (err, reviewsResults) => {
       if (err) return handleFailQuery(err, res);
       movie.reviews = reviewsResults;
+      movie.image = buildMovieImgPath(movie.image);
       res.json({ movie });
     });
   });
 }
 
 module.exports = { index, show };
+
+// Path per immagini
+function buildMovieImgPath(image) {
+  return `${process.env.APP_URL}:${process.env.APP_PORT}/img/${image}`;
+}
