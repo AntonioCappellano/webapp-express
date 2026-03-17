1a8cefce9f3176b8c31f5160a3aa26656a708dd0
AntonioCappellano <antoniocappellano97@gmail.com>
2026-03-17 15:23:59 +0100

    add cors policy

--- app.js
+++ app.js
@@ -2,25 +2,26 @@ const express = require("express");
 const app = express();
 
 //MIDDLEWARES
+const cors = require("cors");
+
 const logger = require("./middlewares/logger");
+
 app.use(logger);
 app.use(express.static("public"));
+app.use(cors({ origin: "http://localhost:5173"}));
 app.use(express.json());
 
 // ROUTERS
-const globalRouter = require("./routers/globalRouters")
-const movieRouter = require("./routers/moviesRouters")
+const globalRouter = require("./routers/globalRouters");
+const movieRouter = require("./routers/moviesRouters");
 
-app.use(globalRouter)
-app.use("/movies", movieRouter)
+app.use(globalRouter);
+app.use("/movies", movieRouter);
 
 //ERROR HANDLING
 const errorMiddleware = require("./middlewares/errorHandler");
 app.use(errorMiddleware.error404);
 app.use(errorMiddleware.error500);
-
-
-
 
 app.listen(process.env.APP_PORT, () => {
   console.log(
